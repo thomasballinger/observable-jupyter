6bf677cecaa533d39e36f5ebfe11487d8cbfb743
Thomas Ballinger <thomasballinger@gmail.com>
2022-07-24 18:55:19 -0700

    first attempt at readthedocs

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,19 +1,24 @@
+Clone the repo on GitHub and clone it so you have a copy on your local machine.
+
+Make sure you have Python 3.10 installed.
+Make sure you have Node.js installed.
+
+OPTIONAL: create a virtual environment and activate it.
+
 Install project automation tool [`nox`](https://nox.thea.codes/en/stable/):
+This runs commands in new virtualenvironments.
 
 ```
-python -m pip install --user nox
+python -m pip install nox
 ```
 
 ### Unit Tests
 
 ```
 nox -s tests
-```
-
-### Lint
-
-```
-nox -s lint
+nox -s js_tests
+nox -s jupyter_notebook_manual_test
+nox -s jupyter_lab_manual_test
 ```
 
 ### Publish to PyPI
@@ -21,3 +26,4 @@ nox -s lint
 ```
 nox -s publish
 ```
+
--- noxfile.py
+++ noxfile.py
@@ -3,9 +3,9 @@ from pathlib import Path
 
 nox.options.sessions = ["tests"]
 
-python = ["3.6", "3.7", "3.8", "3.9"]
+python = ["3.7", "3.8", "3.9", "3.10"]
 
-@nox.session(python="3.9")
+@nox.session(python="3.10")
 def tests(session):
     session.install("-e", ".[test]", )
     tests = session.posargs or ["tests"]
@@ -23,31 +23,31 @@ def tests_on_all_versions(session):
         *tests
     )
 
-@nox.session(python="3.9")
+@nox.session(python="3.10")
 def js_tests(session):
     session.run("npm", "--prefix", "js", "--silent", "install", external=True)
     session.run("npm", "--prefix", "js", "test", external=True)
 
-@nox.session(python="3.9")
+@nox.session(python="3.10")
 def edit_example(session):
     """Open the example Jupyter notebook with a dev install of this module"""
     session.install("-e", ".", "Jupyter")
     session.run("Jupyter", "notebook", "Observable_Embed_Example.ipynb")
 
-@nox.session(python="3.9")
-def jupyter_notebook_test(session):
+@nox.session(python="3.10")
+def jupyter_notebook_manual_test(session):
     """Open the example Jupyter notebook with a dev install of this module"""
     session.install("-e", ".", "jupyter")
     session.run("jupyter", "notebook", "test_notebook.ipynb")
 
-@nox.session(python="3.9")
-def jupyter_lab_test(session):
+@nox.session(python="3.10")
+def jupyter_lab_manual_test(session):
     """Open the example Jupyter notebook with a dev install of this module"""
     session.install("-e", ".", "jupyterlab")
     session.run("jupyter", "lab", "test_notebook.ipynb")
 
 # there's not c extension, so we don't need a separate build per Python version!
-@nox.session(python="3.9")
+@nox.session(python="3.10")
 def build(session):
     session.install("setuptools")
     session.install("wheel")
@@ -55,7 +55,14 @@ def build(session):
     session.run("rm", "-rf", "dist", "build", external=True)
     session.run("python", "setup.py", "--quiet", "sdist", "bdist_wheel")
 
-@nox.session(python="3.9")
+@nox.session(python="3.10")
+def docs(session):
+    session.install("-r", "docs/requirements.txt")
+    with session.cd("docs"):
+        session.run('make', 'html')
+
+
+@nox.session(python="3.10")
 def publish(session):
     build(session)
     print("REMINDER: Has the changelog been updated?")
